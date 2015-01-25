df529acfc8b4e5dc7df477bbbeaeb2f929a261b2
Brendan <brendan.cass@gmail.com>
2015-01-24 22:20:02 -0600

    FUUUUUU

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10;
+
+	void Awake () {
+		rigidbody.AddForce (Vector3.up * 500);
+		}
+
+	void Update(){
+
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+}
